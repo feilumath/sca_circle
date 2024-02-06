d5294db0c40bd6e3a473ba2ddb3c69c5a6595c25
feilu <feilumath@gmail.com>
2024-02-05 19:02:03 -0500

    Update prob_transit_matrix.m
    compute l1 ratios

--- 1d_toy/prob_transit_matrix.m
+++ 1d_toy/prob_transit_matrix.m
@@ -28,14 +28,14 @@ end
 %% test dependence on T: adding perturbations to T, and test changes in Pmat and pi
 nsimu  = 100; 
 plotON = 1; 
-[diff_Tmat,diff_Pmat,diff_pi]= dependence_on_T(stoCA_par,K,N,nsimu,plotON); 
+[diff_Tmat,diff_Pmat,diff_pi,diff_l1]= dependence_on_T(stoCA_par,K,N,nsimu,plotON); 
 
 
 %% a sequence of N: 
- Nseq = 3:9; K    = 2; 
-% Nseq = 3:7; K    = 3; 
+% Nseq = 3:9; K    = 2; 
+ Nseq = 3:7; K    = 3; 
 
-datafilename = ['dependence_on_T_varyN_K',sprintf('%i',K),'.mat'];
+datafilename = [SAVE_DIR,'dependence_on_T_varyN_K',sprintf('%i',K),'_l1.mat'];
 if ~exist(datafilename,'file')
     n_Nseq      = length(Nseq);
     diff_Tseq   = zeros(n_Nseq,nsimu);
@@ -47,7 +47,7 @@ if ~exist(datafilename,'file')
         N  = Nseq(nn);
         nhbrSize = min(3,floor(N/2));  %% in both left and right;   neighbor size = 2*nhbrSize+1;
         stoCA_par = settings_model(K,N,tN,nhbrSize);      % settings of the SCA model: N, K, graph, etc,
-        [diff_Tseq(nn,:),diff_Pseq(nn,:),diff_pi_seq(nn,:)]= dependence_on_T(stoCA_par,K,N,nsimu,0);
+        [diff_Tseq(nn,:),diff_Pseq(nn,:),diff_pi_seq(nn,:),diff_l1]= dependence_on_T(stoCA_par,K,N,nsimu,0);
     end
 
     ratio_PT       = diff_Pseq./diff_Tseq;
@@ -55,22 +55,44 @@ if ~exist(datafilename,'file')
     mean_ratio_PT  = mean(ratio_PT,2);
     mean_ratio_piT = mean(ratio_piT,2);
 
-    save(datafilename,'Nseq','mean_ratio_PT','mean_ratio_piT', "K","diff_pi_seq","diff_Tseq","diff_Pseq",'nsimu');
+% 
+%    diff_l1.diff_Tmat_l1 = diff_Tmat1_l1; 
+% diff_l1.diff_Pmat_l1 = diff_Pmat1_l1; 
+% diff_l1.diff_pi_l1    = diff_pi_l1; 
+    ratio_PT_l1       = diff_l1.diff_Pmat_l1./diff_l1.diff_Tmat_l1;
+    ratio_piT_l1      = diff_l1.diff_pi_l1./diff_l1.diff_Tmat_l1;
+    mean_ratio_PT_l1  = mean(ratio_PT_l1,2);
+    mean_ratio_piT_l1 = mean(ratio_piT_l1,2);
+    
+    save(datafilename,'Nseq','mean_ratio_PT','mean_ratio_piT', "K","diff_pi_seq","diff_Tseq","diff_Pseq",'nsimu',...
+        'mean_ratio_PT_l1',"mean_ratio_piT");
 end
 
 load(datafilename); 
 
 figure; 
+subplot(1,2,1)
 semilogy(Nseq,mean_ratio_PT,'--x','linewidth',2); hold on
 semilogy(Nseq,mean_ratio_piT,'-.d','linewidth',2); 
 semilogy(Nseq,mean_ratio_piT(1)*K.^(-( Nseq-Nseq(1)) /2),':','linewidth',2);
-legend('|P-diff|/|T-dff|','|pi-diff|/|T-dff|','c_0 K^{-N/2}')
+legend('|P_1-P_2|_2/|T_1-T_2|_2','|\pi_1-\pi_2|_2/|T_1-T_2|_2','c_0 K^{-N/2}')
 xlabel(['N (with K=',sprintf('%i)',K)] );
 ylabel('Mean in 100 simulations');
 
+subplot(1,2,2)
+semilogy(Nseq,mean_ratio_PT_l1,'--x','linewidth',2); hold on
+semilogy(Nseq,mean_ratio_piT_l1,'-.d','linewidth',2); 
+semilogy(Nseq,mean_ratio_piT_l1(1)*K.^(-( Nseq-Nseq(1)) /2),':','linewidth',2);
+legend('|P_1-P_2|_1/|T_1-T_2|_1','|\pi_1-\pi_2|_1/|T_1-T_2|_1','c_0 K^{-N/2}')
+xlabel(['N (with K=',sprintf('%i)',K)] );
+% ylabel('Mean in 100 simulations');
 
-figname = ['mean_ratio_varyN_K',sprintf('%i',K)];
+
+figname = [SAVE_DIR,'mean_ratio_varyN_K',sprintf('%i_l1',K)];
 set_positionFontsAll;
+
+
+
 %%% ============ 
 
 
